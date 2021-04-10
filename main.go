309ffbb99073be0cad87d95b2a5d58386329d3a9
jordanm <jordanm@wicrenet.com>
2021-04-10 09:53:06 +0800

    移除 URL 路径中的斜杆

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"github.com/gorilla/mux"
+	"strings"
 )
 
 func homeHandler(w http.ResponseWriter,r *http.Request) {
@@ -43,6 +44,16 @@ func forceHTMLMiddleware(next http.Handler) http.Handler {
 	})
 }
 
+func removeTrailingSlash(next http.Handler) http.Handler {
+	return http.HandlerFunc(func (w http.ResponseWriter,r *http.Request) {
+		// 1. 除首页以外，移除所有请求路径后面的斜杆
+		if r.URL.Path != "/" {
+			r.URL.Path = strings.TrimSuffix(r.URL.Path,"/")
+		}
+		// 2. 将请求传递下去
+		next.ServeHTTP(w,r)
+	})
+}
 
 
 func main()  {
@@ -67,5 +78,5 @@ func main()  {
 	articleURL,_ := router.Get("articles.show").URL("id","23555")
 	fmt.Println("articleURL:",articleURL)
 
-	http.ListenAndServe(":3000",router)
+	http.ListenAndServe(":3000",removeTrailingSlash(router))
 }
