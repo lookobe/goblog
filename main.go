1744473c98180daf5bbc720ec026057beeed26af
jordanm <jordanm@wicrenet.com>
2021-04-12 15:32:58 +0800

    重构 Int64ToString

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"unicode/utf8"
 	"goblog/route"
 	"goblog/logger"
+	"goblog/types"
 
 
 	"github.com/go-sql-driver/mysql"
@@ -114,11 +115,6 @@ func getArticleByID(id string) (Article, error) {
 
 }
 
-
-func Int64ToString(num int64) string {
-	return strconv.FormatInt(num,10)
-}
-
 func articlesShowHandle(w http.ResponseWriter, r *http.Request) {
 	// 1. 获取 URL 参数
 	id := route.GetRouterVariable("id", r)
@@ -141,7 +137,7 @@ func articlesShowHandle(w http.ResponseWriter, r *http.Request) {
 		// 4.读取成功,显示文章
 		tmpl,err := template.New("show.gohtml").Funcs(template.FuncMap{
 			"RouteName2URL": route.Name2URL,
-			"Int64ToString": Int64ToString,
+			"Int64ToString": types.Int64ToString,
 		}).ParseFiles("resources/views/articles/show.gohtml")
 		logger.LogError(err)
 		tmpl.Execute(w, article)
