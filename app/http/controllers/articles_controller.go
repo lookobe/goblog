1a21cdbdda7a3de7f8e6a49cc27c795de1a0203e
jordanm <jordanm@wicrenet.com>
2021-04-16 13:25:51 +0800

    文章内容页

--- app/http/controllers/articles_controller.go
+++ app/http/controllers/articles_controller.go
@@ -41,12 +41,22 @@ func (*ArticlesController) Show(w http.ResponseWriter,r *http.Request) {
 		}
 	}else{
 		// 4. 读取成功，显示文章
-		tmpl,err := template.New("show.gohtml").Funcs(template.FuncMap{
-			"RouteName2URL": route.Name2URL,
-			"Int64ToString": types.Int64ToString,
-		}).ParseFiles("resources/views/articles/show.gohtml")
-		logger.LogError(err)
-		tmpl.Execute(w,article)
+        // 4.0 设置模板相对路径
+        viewDir := "resources/views"
+        // 4.1 所有布局模板文件 Slice
+        files,err := filepath.Glob(viewDir + "/layouts/*.gohtml")
+        logger.LogError(err)
+        // 4.2 在Slice里新增我们的目标文件
+        newFiles := append(files,viewDir+"/articles/show.gohtml")
+        // 4.3 解析模板文件
+        tmpl,err := template.New("show.gohtml").Funcs(template.FuncMap{
+            "RouteName2URL": route.Name2URL,
+            "Int64ToString": types.Int64ToString,
+        }).ParseFiles(newFiles...)
+        logger.LogError(err)
+
+		// 4.4 渲染模板，将所有的文章的数据传输进去
+        tmpl.ExecuteTemplate(w,"app",article)
 	}
 }
 
