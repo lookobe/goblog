d876ee3cfff03c98fb0fbac41263916ec10156ef
jordanm <jordanm@wicrenet.com>
2021-04-17 17:12:53 +0800

    美化文章表单页

--- app/http/controllers/articles_controller.go
+++ app/http/controllers/articles_controller.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 	"unicode/utf8"
 	"goblog/pkg/route"
@@ -39,7 +38,7 @@ func (*ArticlesController) Show(w http.ResponseWriter,r *http.Request) {
 		}
 	}else{
 		// 4. 读取成功，显示文章
-        view.Render(w,"articles.show",article)
+        view.Render(w,article,"articles.show")
 	}
 }
 
@@ -54,31 +53,20 @@ func (*ArticlesController) Index(w http.ResponseWriter,r *http.Request) {
 		fmt.Fprint(w,"500 服务器内部错误")
 	} else {
 		// 2. 加载模版
-		view.Render(w,"articles.index",articles)
+		view.Render(w,articles,"articles.index")
 	}
 }
 
 // ArticleFormData 创建博文表单数据
 type ArticlesFormData struct {
 	Title,Body string
-	URL string
+	Article article.Article
 	Errors map[string]string
 }
 
 // Create 文章创建页面
 func (*ArticlesController) Create(w http.ResponseWriter,r *http.Request) {
-	storeURL :=route.Name2URL("articles.store")
-	data :=  ArticlesFormData {
-		Title: "",
-		Body: "",
-		URL: storeURL,
-		Errors: nil,
-	}
-	tmpl,err := template.ParseFiles("resources/views/articles/create.gohtml")
-	if err != nil {
-		panic(err)
-	}
-	tmpl.Execute(w,data)
+    view.Render(w,ArticlesFormData{},"articles.create","articles._form_field")
 }
 
 func validateArticleFormData(title string,body string) map[string]string {
@@ -117,16 +105,11 @@ func (*ArticlesController) Store (w http.ResponseWriter, r *http.Request) {
 			fmt.Fprint(w,"创建文章失败，请联系管理员")
 		}
 	}else {
-		storeURL := route.Name2URL("articles.store")
-		data := ArticlesFormData{
-			Title: title,
-			Body: body,
-			URL: storeURL,
-			Errors: errors,
-		}
-		tmpl,err := template.ParseFiles("resources/views/articles/create.gohtml")
-		logger.LogError(err)
-		tmpl.Execute(w,data)
+        view.Render(w,ArticlesFormData{
+            Title: title,
+            Body: body,
+            Errors: errors,
+        },"articles.create","articles,_form_field")
 	}
 }
 // Edit 文章更新页面
@@ -135,7 +118,7 @@ func (*ArticlesController) Edit(w http.ResponseWriter, r *http.Request) {
 	id := route.GetRouterVariable("id",r)
 
 	// 2. 读取对应的文章数据
-	article,err := article.Get(id)
+	_article,err := article.Get(id)
 
 	// 3. 如果出现错误
 	if err != nil {
@@ -151,16 +134,12 @@ func (*ArticlesController) Edit(w http.ResponseWriter, r *http.Request) {
 		}
 	}else {
 		// 4. 读取成功，显示编辑文章表单
-		updateURL := route.Name2URL("articles.update","id",id)
-		data := ArticlesFormData {
-			Title: article.Title,
-			Body: article.Body,
-			URL: updateURL,
-			Errors: nil,
-		}
-		tmpl,err := template.ParseFiles("resources/views/articles/edit.gohtml")
-		logger.LogError(err)
-		tmpl.Execute(w,data)
+		view.Render(w, ArticlesFormData{
+            Title: _article.Title,
+            Body: _article.Body,
+            Article: _article,
+            Errors: nil,
+        },"articles.edit","articles._form_field")
 	}
 }
 
@@ -209,16 +188,12 @@ func (*ArticlesController) Update(w http.ResponseWriter, r *http.Request) {
 			}
 		}else {
 			// 4.3 表单验证不通过，显示理由
-			updateURL := route.Name2URL("articles.update","id",id)
-			data := ArticlesFormData{
-				Title: title,
-				Body: body,
-				URL: updateURL,
-				Errors: errors,
-			}
-			tmpl,err := template.ParseFiles("resources/views/articles/edit.gohtml")
-			logger.LogError(err)
-			tmpl.Execute(w,data)
+			view.Render(w, ArticlesFormData{
+                Title: title,
+                Body: body,
+                Article: _article,
+                Errors: errors,
+            },"articles.edit","articles._form_field")
 		}
 	}
 }
