562839d450a75e94245844985e027f478b81cca6
jordanm <jordanm@wicrenet.com>
2021-04-27 15:48:43 +0800

    文章作者

--- app/models/article/curd.go
+++ app/models/article/curd.go
@@ -9,7 +9,7 @@ import (
 func Get(idstr string) (Article,error) {
 	var article Article
 	id := types.StringToInt(idstr)
-	if err := model.DB.First(&article,id).Error;err != nil {
+	if err := model.DB.Preload("User").First(&article,id).Error;err != nil {
 		return article,err
 	}
 	return article,nil
@@ -18,7 +18,7 @@ func Get(idstr string) (Article,error) {
 // GetAll 获取全部文章
 func GetAll() ([]Article,error) {
 	var articles []Article
-	if err := model.DB.Find(&articles).Error; err != nil {
+	if err := model.DB.Preload("User").Find(&articles).Error; err != nil {
 		return articles,err
 	}
 	return articles,nil
